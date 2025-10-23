3a57a3dde2a2820390f3905d457e6cc06b8bbe12
YVC-Michael <MRyan328@student.yvcc.edu>
2025-10-23 09:08:15 -0700

    Complete Chapter 5 Programming Exercises 1 and started 2

--- Chapter5-ProgrammingExercises-01.py
+++ Chapter5-ProgrammingExercises-01.py
@@ -5,3 +5,21 @@
 
 # Miles 5 Kilometers 3 0.6214
 
+# Constants
+KILOMETERS_TO_MILES = 0.6214
+
+# User Description
+print()
+print("This program will convert kilometers to miles.")
+print()
+
+# Get user input for kilometers
+kilometers = float(input("Enter a distance in kilometers: "))
+
+# Convert kilometers to miles
+miles = kilometers * KILOMETERS_TO_MILES
+
+# Display miles
+print()
+print(f"{kilometers} kilometers is equal to {miles:.3f} miles.")
+print()
--- Chapter5-ProgrammingExercises-02.py
+++ Chapter5-ProgrammingExercises-02.py
@@ -5,3 +5,27 @@
 ### on a purchase. If you have already written that program, redesign it so the subtasks are in
 ### functions. If you have not already written that program, write it using functions.
 
+# Named Constants
+SALES_TAX_STATE = 0.05
+SALES_TAX_COUNTY = 0.025
+
+# Print Prompt
+print('Enter the amount of a purchase: ')
+purchase_amount = float(input())
+
+# Calculate Sales Tax
+sales_tax_state = purchase_amount * SALES_TAX_STATE
+sales_tax_county = purchase_amount * SALES_TAX_COUNTY
+
+# Calculate Total Sales Tax
+sales_tax_total = sales_tax_state + sales_tax_county
+
+# Calculate Total Sale
+sale_total = purchase_amount + sales_tax_total
+
+# Print Results
+print(f'The amount of the purchase is:\t${purchase_amount:10,.2f}')
+print(f'The state sales tax is:\t\t${sales_tax_state:10,.2f}')
+print(f'The county sales tax is:\t\t${sales_tax_county:10,.2f}')
+print(f'The total sales tax is:\t\t${sales_tax_total:10,.2f}')
+print(f'The total of the sale is:\t\t${sale_total:10,.2f}')
