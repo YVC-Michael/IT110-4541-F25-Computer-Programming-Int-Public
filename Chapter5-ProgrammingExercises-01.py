a601b3f5328e98d203e4e55dfe02eb42e49d3af4
YVC-Michael <MRyan328@student.yvcc.edu>
2025-10-23 08:40:06 -0700

    Add Chapter 5 Programming Exercises - Initial setup with problem descriptions- Added exercises 1-7, 9, 10, 15, and 21- Files contain problem statements ready for implementation- Part of IT110 Fall 2025 coursework

---
+++ Chapter5-ProgrammingExercises-01.py
@@ -0,0 +1,7 @@
+###1. Kilometer Converter
+
+### Write a program that asks the user to enter a distance in kilometers, then converts that
+### distance to miles. The conversion formula is as follows:
+
+# Miles 5 Kilometers 3 0.6214
+
