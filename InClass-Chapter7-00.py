1b14840af74eae494c93262a7de356c51e590cb6
YVC-Michael <MRyan328@student.yvcc.edu>
2025-11-06 10:39:44 -0800

    Add InClass-Chapter7-00.py

---
+++ InClass-Chapter7-00.py
@@ -0,0 +1,15 @@
+#Class Notes Chapter 7
+# 11/06/2025
+
+mylist = [2,3,4,5,6,7]
+print(len(mylist))
+
+print(8 in mylist)
+
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6)
+thislist = mylist.copy()
+print(thislist)
+
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
