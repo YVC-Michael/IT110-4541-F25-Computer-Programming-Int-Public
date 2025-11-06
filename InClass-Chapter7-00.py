38faa78a4d81637ed75ab93f784e92fca217b299
YVC-Michael <MRyan328@student.yvcc.edu>
2025-11-06 10:55:16 -0800

    Add list and tuple conversion examples

--- InClass-Chapter7-00.py
+++ InClass-Chapter7-00.py
@@ -7,6 +7,15 @@ print(len(mylist))
 print(8 in mylist)
 
 thistuple = (1, 3, 7, 8, 7, 5, 4, 6)
+tuple_to_list = list(thistuple)
+print(tuple_to_list)
+
+list_to_tuple = tuple(mylist)
+print(list_to_tuple)
+
+thislist = list(mylist)
+print(thislist)
+
 thislist = mylist.copy()
 print(thislist)
 
